7e494589b1a8ae485cabd58e4a6b624c6fed7858
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-09-30 18:58:17 -0400

    fixed this mistake

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-ReactDOM(<App/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
