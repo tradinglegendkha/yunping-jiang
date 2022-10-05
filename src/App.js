51586f9b1e4ef5fc7b2c479efa4cfaabeabcb3ae
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-10-05 19:32:48 -0400

    wrapped up header, might need to add more

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 const App = () => {
     return (
         <div className="App">
-            <div className="gradient_bg">
+            <div className="gradient-bg">
                 <Navbar/>
                 <Header/>
             </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './header.css';
+import jiang from '../../assets/jiang.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header section-padding' id='home'>
+      <div className='header-content'>
+        <h1 className='gradient-text'>The Distinguished Professor of Mathematics</h1>
+        <p>At Queens College and Graduate Center of the City University of New York</p>
+      </div>
+      <div className='header-image'>
+        <img src={jiang} alt='jiang'/>
+      </div>
+    </div>
   )
 }
 
