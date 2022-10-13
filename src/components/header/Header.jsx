fd4b6764e309ae9665e3e8b96904316874132eef
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-10-13 12:24:06 -0400

    added typing animation welcome

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,6 +8,9 @@ const Header = () => {
       <div className='header-content'>
         <h1 className='gradient-text'>The Distinguished Professor of Mathematics</h1>
         <p>At Queens College and Graduate Center of the City University of New York</p>
+        <p1>Hello, I am Yunping Jiang</p1>
+        <p2>Welcome to my class</p2>
+        
       </div>
       <div className='header-image'>
         <img src={jiang} alt='jiang'/>
