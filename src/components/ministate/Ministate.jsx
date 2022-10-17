481055223fc3047f668fc5b88919b5900b27364a
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-10-17 15:25:32 -0400

    small grammer fix

--- src/components/ministate/Ministate.jsx
+++ src/components/ministate/Ministate.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './ministate.css';
+import React from "react";
+import "./ministate.css";
 
 const Ministate = () => {
   return (
-    <div className='ministate-container'>
-      <div className='ministate-container-statement'>
-        <p>Taught for 20 years, for thousands of students</p>
+    <div className="ministate-container">
+      <div className="ministate-container-statement">
+        <p>Taught for 20 years, for thousand students</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Ministate
+export default Ministate;
