f09182226db760ada35b2febb2c2c8526b7f7296
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-10-18 19:05:14 -0400

    moved the text to a js file, need to fix

--- src/components/current/Current.jsx
+++ src/components/current/Current.jsx
@@ -1,54 +1,6 @@
 import React from "react";
 import "./current.css";
-
-let text11 =
-  "Zero entropy continuous interval maps and MMLS-MMA property. Nonlinearity 31 (2018) 2689–2702. https://doi.org/10.1088/1361-6544/aab593";
-let text12 =
-  "Oscillating sequences, MMA and MMLS flows and Sarnak’s conjecture (with Aihua Fan). Ergod. Th. & Dynam. Sys., August 2018, Vol. 38, no. 5, 1709–1744. https://doi.org/10.1017/etds.2016.121";
-let text13 =
-  "Higher order oscillation and uniform distribution (with Shigeki Akiyama). Uniform Distribution Theory, Volume 14 (2019), no. 1, 1-10. https://doi.org/10.2478/udt-2019-0001";
-let text14 =
-  "Orders of oscillation motivated by Sarnaks conjecture. Proceedings of AMS, Volume 147, Number 7, July 2019, 3075-3085. https://doi.org/10.1090/proc/14487";
-let text21 =
-  "Cycle doubling, merging and renormalization in the tangent family (with Tao Chen and Linda Keen). Conformal Geometry and Dynamics, AMS, Volume 22 (2018), 271–314. https://doi.org/10.1090/ecgd/327";
-let text22 =
-  "Slices of parameter space for meromorphic maps with two asymptotic values (with Tao Chen and Linda Keen). Ergodic Theory and Dynamical Systems , First View , pp. 1 – 41, DOI: https://doi.org/10.1017/etds.2021.108. aXiv Preprint https://arxiv.org/pdf/1908.06028.pdf";
-let text23 =
-  "Accessible Boundary Points in the Shift Locus of a Family of Meromorphic Functions with Two Finite Asymptotic Values (with Tao Chen and Linda Keen). Arnold Mathematical Journal. Online First, January 7, 2021. https://doi.org/10.1007/s40598-020-00169-1";
-let text24 =
-  "A framework towards understanding the characterization of holomorphic maps (an Appendix on Transcendental Functions by Tao Chen, Yunping Jiang, and Linda Keen). Frontiers in Complex Dynamics, March 2014, Princeton University Press, 235-253. http://www.jstor.org/stable/j.ctt5vjv7b.15";
-let text31 =
-  "Global graph of metric entropy on expanding Blaschke products. Discrete and Continuous Dynamical Systems, Online First, September 2020. http://dx.doi.org/10.3934/dcds.2020325";
-let text32 =
-  "Infimum of the metric entropy of volume preserving Anosov systems (with Huyi Hu and Miaohua Jiang). Discrete and Continuous Dynamical Systems, Volume 37 (2017), No. 9, 4767-4783. http://dx.doi.org/10.3934/dcds.2017205";
-let text33 =
-  "Infimum of the metric entropy of hyperbolic attractors with respect to the SRB measure (with Huyi Hu and Miaohua Jiang). Discrete and Continuous Dynamical Systems, Vol. 22, No. 1&2 (2008), 215–234. http://dx.doi.org/10.3934/dcds.2008.22.215";
-let text34 =
-  "Distance entropy of dynamical systems on noncompact phase spaces (with Xiongping Dai). Discrete and Continuous Dynamical Systems, Vol. 20, No. 2 (2008), 313-333. http://dx.doi.org/10.3934/dcds.2008.20.313";
-let text41 =
-  "Geometric Gibbs theory. Science China Mathematics, September 2020, Vol. 63, no. 9, 1777-1824.  https://doi.org/10.1007/s11425-019-1638-6";
-let text42 =
-  "Symmetric rigidity for circle endomorphisms with bounded geometry (with John Adamski, Yunchun Hu, Yunping Jiang, and Zhe Wang). The Proceedings of the American Mathematical Society, 150 (2022), 3581-3593. https://www.ams.org/journals/proc/2022-150-08/S0002-9939-2022-15921-8/home.html.  https://arxiv.org/pdf/2101.06870.pdf";
-let text43 =
-  "Symmetric invariant measures.  Contemporary Mathematics, AMS, Vol. 575, 2012, 211-218. http://dx.doi.org/10.1090/conm/575/11399";
-let text44 =
-  "A function model for the Teichmüller space of a closed hyperbolic Riemann surface. Science China Mathematics, November 2019, Vol. 62, No. 11, 2249-2270. https://doi.org/10.1007/s11425-019-9520-4";
-let text51 =
-  "Holomorphic motions and related topics (with Frederick Gardiner and Zhe Wang). Geometry of Riemann Surfaces, London Mathematical Society Lecture Note Series, No. 368, 2010, 156-193.";
-let text52 =
-  "Winding numbers and full extendibility in holomorphic motions. Conformal Geometry and Dynamics, AMS, May 26, 2020, Vol. 24, 109-117. https://doi.org/10.1090/ecgd/351";
-let text53 =
-  "Monodromy, liftings of holomorphic maps, and extensions of holomorphic motions (with Sudeb Mitra). Conformal Geometry and Dynamics, Volume 22 (2018), 333–344. https://doi.org/10.1090/ecgd/329";
-let text54 =
-  "Teichmüller spaces and tame quasiconformal motions (with Sudeb Mitra, Hiroshige Shiga, and Zhe Wang). Tohoku Mathematical Journal, The Second Series, Vol. 70 (2018), No. 4, 607-631.";
-let text61 =
-  "Nanjing Lecture Notes in Dynamical Systems. Part One: Transfer Operators in Thermodynamical Formalism. FIM Preprint Series, ETH-Zurich, June 2000.";
-let text62 =
-  "Convergence speed of a Ruelle operator associated with a non-uniformly expanding conformal dynamical system and a Dini potential. Discrete and Continuous Dynamical Systems (with Yuan-Ling Ye). Discrete and Continuous Dynamical Systems., Volume 38, Number 9 (2018), 4693-4713. http://dx.doi.org/10.3934/dcds.2018206";
-let text63 =
-  "Ruelle operator theorem for nonexpansive systems (with Yuan-Ling Ye). Ergodic Theory and Dynamical Systems, (2010), 30, 469–487. https://doi.org/10.1017/S014338570900025X";
-let text64 =
-  "Decay of correlations for weakly expanding dynamical systems with Dini potentials under optimal quasi-gap condition (with Yuan-Ling Ye). Nonlinearity, Volume 35 (2022), Number 2, 916-953. https://doi.org/10.1088/1361-6544/ac3c2c";
+import allText from "./researchtext.js";
 
 const Current = () => {
   return (
@@ -58,48 +10,48 @@ const Current = () => {
         <div className="current-container-box">
           <div className="boxes">
             <h6>Number Theory and Ergodic Theory</h6>
-            <p>{text11}</p>
-            <p>{text12}</p>
-            <p>{text13}</p>
-            <p>{text14}</p>
+            <p>{allText.text11}</p>
+            <p>{allText.text12}</p>
+            <p>{allText.text13}</p>
+            <p>{allText.text14}</p>
           </div>
           <div className="boxes">
             <h6>Complex Dynamics</h6>
-            <p>{text21}</p>
-            <p>{text22}</p>
-            <p>{text23}</p>
-            <p>{text24}</p>
+            <p>{allText.text21}</p>
+            <p>{allText.text22}</p>
+            <p>{allText.text23}</p>
+            <p>{allText.text24}</p>
           </div>
           <div className="boxes">
             <h6>Metric Entropy in Smooth Dynamical Systems</h6>
-            <p>{text31}</p>
-            <p>{text32}</p>
-            <p>{text33}</p>
-            <p>{text34}</p>
+            <p>{allText.text31}</p>
+            <p>{allText.text32}</p>
+            <p>{allText.text33}</p>
+            <p>{allText.text34}</p>
           </div>
           <div className="boxes">
             <h6>
               Applications of Quasiconformal Mappings and Teichmüller Theory to
               Dynamical Systems and vice versa
             </h6>
-            <p>{text41}</p>
-            <p>{text42}</p>
-            <p>{text43}</p>
-            <p>{text44}</p>
+            <p>{allText.text41}</p>
+            <p>{allText.text42}</p>
+            <p>{allText.text43}</p>
+            <p>{allText.text44}</p>
           </div>
           <div className="boxes">
             <h6>Holomorphic Motions</h6>
-            <p>{text51}</p>
-            <p>{text52}</p>
-            <p>{text53}</p>
-            <p>{text54}</p>
+            <p>{allText.text51}</p>
+            <p>{allText.text52}</p>
+            <p>{allText.text53}</p>
+            <p>{allText.text54}</p>
           </div>
           <div className="boxes">
             <h6>Transfer Operators and Thermodynamical Formalism</h6>
-            <p>{text61}</p>
-            <p>{text62}</p>
-            <p>{text63}</p>
-            <p>{text64}</p>
+            <p>{allText.text61}</p>
+            <p>{allText.text62}</p>
+            <p>{allText.text63}</p>
+            <p>{allText.text64}</p>
           </div>
         </div>
       </div>
